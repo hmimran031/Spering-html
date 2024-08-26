99566fd8fa5e47ab624de1011dbde89dab23957f
hmimran031 <hmimran031@hmail.com>
2024-08-26 18:05:39 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  items: 1,
+  loop: true,
+  margin: 0,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+});
+$(".play").on("click", function () {
+  owl.trigger("play.owl.autoplay", [3000]);
+});
+$(".stop").on("click", function () {
+  owl.trigger("stop.owl.autoplay");
+});
+// -----------
